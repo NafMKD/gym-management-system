c36c6b19bbe01f87dd18383b3ded624ae1c5e846
NafMKD <guchemenberu32@gmail.com>
2026-04-21 09:23:43 +0300

    footer updated

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer text-sm">
     <strong>{{ __("Copyright")}} &copy; <?= date('Y') ?> <a target="_blank"
-            href="https://naflet.com"><b>{{ __("Naflet PLC") }}</a></strong>
+            href="https://github.com/nafMKD"><b>{{ __("@NafMKD") }}</a></strong>
     {{ __("All rights reserved.") }}
     <div class="float-right d-none d-sm-inline-block">
         <b>{{ __("Version") }}</b> 1.0.0
